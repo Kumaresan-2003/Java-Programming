20d103fa44e5bd4730ca4d2e10381dd7e2c11b2a
Kumaresan KR <114505343+Kumaresan-2003@users.noreply.github.com>
2024-06-08 11:59:50 +0530

    Update Addbinarystring .java

--- Training code/Addbinarystring .java
+++ Training code/Addbinarystring .java
@@ -1,36 +1,44 @@
-// // Java program to add two binary strings
+// Java program to add two binary strings
 
-// public class Addbinarystring {
+public class Addbinarystring {
 
-// 	// Function to add two binary strings
-// 	static String add_Binary(String x, String y)
-// 	{
+	// Function to add two binary strings
+	static String add_Binary(String x, String y)
+	{
 
-// 		int num1 = Integer.parseInt(x,2);
-// 		System.out.println(num1);
-// 		// converting binary string into integer( and deto their deccimal
-// 		// number)
+		int num1 = Integer.parseInt(x,2);
+		System.out.println(num1);
+		// converting binary string into integer( and deto their deccimal
+		// number)
 
-// 		int num2 = Integer.parseInt(y, 2);
-// 		// converting binary string into integer(decimal
-// 		// number)
+		int num2 = Integer.parseInt(y, 2);
+			System.out.println(num2);
+		// converting binary string into integer(decimal
+		// number)
 
-// 		int sum = num1 + num2;
-// 		// Adding those two decimal numbers and storing in
-// 		// sum
+		int sum = num1 + num2;
+		// Adding those two decimal numbers and storing in
+		// sum
+			System.out.println("Sum is "+sum);
 
-// 		String result = Integer.toBinaryString(sum);
-// 		// Converting that resultant decimal into binary
-// 		// string
+		String result = Integer.toBinaryString(sum);
+		// Converting that resultant decimal into binary
+		// string
 
-// 		return result;
-// 	}
+		return result;
+	}
 
-// 	// Main driver method
-// 	public static void main(String args[])
-// 	{
-// 		String x = "011011", y = "1010111";
+	// Main driver method
+	public static void main(String args[])
+	{
+		String x = "011011", y = "1010111";
 
-// 		System.out.print(add_Binary(x, y));
-// 	}
-// }
+		System.out.print(add_Binary(x, y));
+	}
+}
+//Input:
+// 27
+// 87
+// Sum is 114
+//OP: 
+//1110010
